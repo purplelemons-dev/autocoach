35554ae083bf0a5a588400bebcbce6eccde213af
purplelemons-dev <ironbreaker2005@gmail.com>
2024-01-17 20:55:33 -0600

    big bannngggg

---
+++ index.ts
@@ -0,0 +1,16 @@
+
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { PORT } = process.env;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on http://localhost:3000');
+});
