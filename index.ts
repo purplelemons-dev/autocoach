07dec33ad33178ad536da0c743b03bdde2e713eb
purplelemons-dev <ironbreaker2005@gmail.com>
2024-01-20 15:48:47 -0600

    downloadmoreram

--- index.ts
+++ index.ts
@@ -75,6 +75,7 @@ app.post("/api/badge", (req, res) => {
         }
         //res.write("Working on users...\n");
         const rows = [];
+        console.log("Working on users...");
         for (const user of db.getUsers().sort((a, b) => a.lastname.localeCompare(b.lastname))) {
             let temp = [];
             temp.push(user.firstname);
