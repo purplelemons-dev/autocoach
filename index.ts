4adc20ca09058f06698e9db64fa01d160d7fbd31
purplelemons-dev <me@joshsmith.dev>
2025-01-19 16:48:47 -0600

    fix: hardcoded hour IDs

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
-
-import express from 'express';
-import dotenv from 'dotenv';
-import { engine } from 'express-handlebars';
-import { API } from './util';
-import { Database, User, Course } from './database';
-import { google, sheets_v4 } from 'googleapis';
+import express from "express";
+import dotenv from "dotenv";
+import { engine } from "express-handlebars";
+import { API } from "./util";
+import { Course, Database, User } from "./database";
+import { google, sheets_v4 } from "googleapis";
 
 dotenv.config();
 const PORT = 3344;
@@ -15,12 +14,12 @@ const mckinneySheetID = process.env.MCKINNEY;
 const rockwallSheetID = process.env.ROCKWALL;
 const hourByCampus = {
     McKinney: [1, 2, 3, 4, 5, 6],
-    Rockwall: [1, 2, 3, 4, 5, 6, 7, 8]
-}
+    Rockwall: [1, 2, 3, 4, 5, 6, 7, 8],
+};
 
 const sheets = new sheets_v4.Sheets({
     auth: new google.auth.GoogleAuth({
-        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+        scopes: ["https://www.googleapis.com/auth/spreadsheets"],
         keyFile: "google_creds.json",
     }),
 });
@@ -34,10 +33,10 @@ const hourTranslate = (hourname: string, campus: string) => {
 };
 
 const app = express();
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-app.use(express.static('public'));
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/test", async (req, res) => {
@@ -45,10 +44,10 @@ app.get("/test", async (req, res) => {
     res.send("hello!");
 });
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
     console.log("GET /");
-    console.log(JSON.stringify(api.fetchOptions(), null, 2))
-    res.render('home', {
+    console.log(JSON.stringify(api.fetchOptions(), null, 2));
+    res.render("home", {
         campus: await api.getCampuses(),
         semester: ["Fall", "Spring"],
     });
@@ -63,7 +62,7 @@ app.post("/api/badge", (req, res) => {
     res.setHeader("Timeout", "360");
     res.flushHeaders();
 
-    api.getHours(campus, semester).then(async hours => {
+    api.getHours(campus, semester).then(async (hours) => {
         if (!hours) return;
         let newHours: {
             hourid: string;
@@ -71,15 +70,15 @@ app.post("/api/badge", (req, res) => {
         }[] = [];
         if (campus === "McKinney") {
             newHours = [
-                { hourid: "21", hourname: "Hour 1: McKinney" },
-                { hourid: "22", hourname: "Hour 2: McKinney" },
-                { hourid: "23", hourname: "Hour 3: McKinney" },
-                { hourid: "24", hourname: "Hour 4: McKinney" },
-                { hourid: "25", hourname: "Hour 5: McKinney" },
-                { hourid: "26", hourname: "Hour 6: McKinney" },
-            ]
+                { hourid: "47", hourname: "Hour 1: McKinney" },
+                { hourid: "49", hourname: "Hour 2: McKinney" },
+                { hourid: "50", hourname: "Hour 3: McKinney" },
+                { hourid: "51", hourname: "Hour 4: McKinney" },
+                { hourid: "52", hourname: "Hour 5: McKinney" },
+                { hourid: "53", hourname: "Hour 6: McKinney" },
+            ];
         }
-        else if (campus === "Rockwall") {
+        /*else if (campus === "Rockwall") {
             newHours = [
                 { hourid: "57", hourname: "Hour 0: Rockwall" },
                 { hourid: "59", hourname: "Hour 1: Rockwall" },
@@ -90,20 +89,29 @@ app.post("/api/badge", (req, res) => {
                 { hourid: "64", hourname: "Hour 6: Rockwall" },
                 { hourid: "65", hourname: "Hour 7: Rockwall" },
             ]
-        }
+        }*/
         for (const { hourid, hourname } of newHours) {
-            console.log(`Working on ${hourname}...`)
+            console.log(`Working on ${hourname}...`);
             const courses = await api.getCoursesFromHour(campus, hourid);
             for (const course of courses) {
                 if (!course) continue;
                 const courseid = course.courseid;
-                const courseObj = new Course(courseid, course.roomnum, course.coursename, hourname);
+                const courseObj = new Course(
+                    courseid,
+                    course.roomnum,
+                    course.coursename,
+                    hourname,
+                );
                 const users = await api.usersInCourse(courseid);
                 //console.log(`DEBUG\tWorking on ${course.coursename}...`);
                 for (const user of users) {
                     let userObj = db.getUser(user.id);
                     if (!userObj) {
-                        userObj = new User(user.id, user.firstname, user.lastname);
+                        userObj = new User(
+                            user.id,
+                            user.firstname,
+                            user.lastname,
+                        );
                         db.addUser(userObj);
                     }
                     userObj.addCourse(courseObj);
@@ -113,7 +121,11 @@ app.post("/api/badge", (req, res) => {
         //res.write("Working on users...\n");
         const rows: any[][] = [];
         console.log("Working on users...");
-        for (const user of db.getUsers().sort((a, b) => a.lastname.localeCompare(b.lastname))) {
+        for (
+            const user of db.getUsers().sort((a, b) =>
+                a.lastname.localeCompare(b.lastname)
+            )
+        ) {
             let temp: string[] = [];
             temp = temp.fill("", 0, campus === "McKinney" ? 14 : 16);
             temp.push(user.firstname);
@@ -146,12 +158,12 @@ app.post("/api/badge", (req, res) => {
                     range: range,
                     valueInputOption: "RAW",
                     requestBody: {
-                        values: rows
-                    }
+                        values: rows,
+                    },
                 }).then(() => {
                     console.log("Sheets Done!");
                     //console.log(res);
-                }).catch(err => {
+                }).catch((err) => {
                     console.log("Sheets Error!");
                     console.log(err);
                 });
@@ -163,7 +175,7 @@ app.post("/api/badge", (req, res) => {
 
 app.post("/api/hours", (req, res) => {
     const { campus, semester } = req.body;
-    api.getHours(campus, semester).then(hours => {
+    api.getHours(campus, semester).then((hours) => {
         res.json(hours);
     });
 });
